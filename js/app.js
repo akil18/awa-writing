266d0be448fc95785e1156d47a4e0a4189ade14c
Akil Chowdhury <akiluddinchy@gmail.com>
2022-08-26 00:26:15 +0600

    word count fixed

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ function postText(collectAreaId, postAreaId, wordCountShow = 0) {
     if(wordCountShow == 1){
         const wordCountField = document.getElementById('word-count');
         wordCountField.parentNode.style.display = "block";
-        const wordCount = writtenText.split('').filter(e => e.trim().length).join('').length;
+        const wordCount = writtenText.split(' ').length;
         wordCountField.innerText = wordCount;  
     }
 
@@ -18,13 +18,15 @@ function postText(collectAreaId, postAreaId, wordCountShow = 0) {
 document.getElementById('postq-btn').addEventListener('click', function(event){
     event.target.disabled = true;
     event.target.style.backgroundColor = "gray";
-
+    document.getElementById('post-question').classList.add('p-2');
+    
     postText('question-area', 'post-question');
 })
 
 document.getElementById('start-btn').addEventListener('click', function(event){
     event.target.disabled = true;
     event.target.style.backgroundColor = "gray";
+    document.getElementById('post-question').classList.add('mb-4');
 
     document.getElementById('answer-area').disabled = false;
     document.getElementById('answer-area').innerText = '';
